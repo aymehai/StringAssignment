3de6d558642dbc0317a931ea5394eb3e32bf346f
student <student@GBTC440J11LX.montgomerycollege.edu>
2017-05-25 14:35:18 -0400

    String Assignment

---
+++ src/StringAssignment.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class StringAssignment {
+
+	public static void main(String[] args) {
+
+		Scanner keyboard = new Scanner(System.in);
+		String name;
+		System.out.println("Type your name");
+		name = keyboard.nextLine();
+		System.out.println("Hello " + name);
+		
+	}
+
+}
